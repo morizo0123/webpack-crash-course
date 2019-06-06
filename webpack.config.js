29d21260c5983bbde6cced1c5d709a9200381731
morizo <morizo0505@gmail.com>
2019-06-06 21:56:03 +0900

    implement css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 
 const outputPath = path.resolve(__dirname, 'dist');
-// console.log({outputPath})
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: outputPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   devServer: {
     contentBase: outputPath
