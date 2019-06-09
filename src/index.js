bc1c9d9b901d0cdd3440ee72f43a6371627fcebe
morizo <morizo0505@gmail.com>
2019-06-09 20:41:49 +0900

    implement babel-loader and react dev

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-import _ from 'lodash'
-import Lion from './utilities'
-import './style.css'
-import './style.scss'
-import bad from './bad.png'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// 衝突を防ぐ
-// import { NAME as NAME_OF_HAM } from './utilities'
-// import * as hoge from './utilities'
-// import { NAME, NiJou } from './utilities'
-
-// console.log(Lion.say())
-
-function component() {
-  const elem = document.createElement('div');
-  const array = ['hello', 'webpack', '!!'];
-  elem.innerHTML = _.join(array, ' ');
-  return elem;
-}
-
-document.body.appendChild(component());
-document.body.classList.add('haikei');
-
-const image = new Image();
-image.src = bad;
-document.body.appendChild(image);
+ReactDOM.render(
+  <div>Hello, React!</div>,
+  document.getElementById('root')
+)
